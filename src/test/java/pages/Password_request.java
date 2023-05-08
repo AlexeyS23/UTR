14ce6d5e56e9d1bdc6e667d132177ce0f59a4a14
unknown <alexsitalo1983@gmail.com>
2023-05-08 22:59:58 +0300

    utr test tasks

--- src/test/java/pages/Password_request.java
+++ src/test/java/pages/Password_request.java
@@ -11,13 +11,13 @@ public class Password_request {
         WebDriver driver=new ChromeDriver();
         driver.get("http://order3-dev.utr.ua:5455");
         driver.manage().window().maximize();
-        WebElement forget = driver.findElement(By.xpath("/html/body/app-root/div/div[2]/div[2]/app-login/div/div[2]/form/div[3]/div[2]/div[1]/button/span[1]"));
+        WebElement forget = driver.findElement(By.xpath("(//*[@class='mat-button-wrapper'])[2]"));
         forget.click();
-        WebElement email = driver.findElement(By.xpath("//*[@id=\"mat-input-2\"]"));
+        WebElement email = driver.findElement(By.xpath("//*[@id='mat-input-2']"));
         email.sendKeys("test@utr.ua");
-        WebElement button = driver.findElement(By.xpath("//button[@class='mat-focus-indicator btn mat-raised-button mat-button-base mat-primary']"));
+        WebElement button = driver.findElement(By.cssSelector("[type='submit']"));
         button.click();
         Thread.sleep(2000);
-        driver.quit();
 
+        driver.quit();
     }}
